143f7df4bf42aff61653a50886e76e2e0d603c5c
Shen-r0n <69506811+Shen-r0n@users.noreply.github.com>
2023-01-03 12:54:19 +0545

    trying to add a swiper but isn't working

--- src/views/home/homePage.vue
+++ src/views/home/homePage.vue
@@ -17,39 +17,42 @@
     </ion-header>
 
     <ion-content color="primary">
-      <!-- <ion-row> -->
-      <swiper>
-        <swiper-slide>
-          <ion-label>
-            <span>Total Balance</span>
-            <p class="acc-amt">
-              <ion-text color="white">Rs 400231300</ion-text>
-            </p>
-            <span>4800080</span>
-          </ion-label>
-        </swiper-slide>
+      <ion-row class="banners">
+        <swiper>
+          <!-- <div> -->
+          <swiper-slide>
+            <ion-label>
+              <span>Total Balance</span>
+              <p class="acc-amt">
+                <ion-text color="white">Rs 400231300</ion-text>
+              </p>
+              <span>4800080</span>
+            </ion-label>
+          </swiper-slide>
 
-        <swiper-slide>
-          <ion-label>
-            <span>Total Balance</span>
-            <p class="acc-amt">
-              <ion-text color="white">Rs 352345123</ion-text>
-            </p>
-            <span>324143080</span>
-          </ion-label>
-        </swiper-slide>
+          <swiper-slide>
+            <ion-label>
+              <span>Total Balance</span>
+              <p class="acc-amt">
+                <ion-text color="white">Rs 352345123</ion-text>
+              </p>
+              <span>324143080</span>
+            </ion-label>
+          </swiper-slide>
 
-        <swiper-slide>
-          <ion-label>
-            <span>Total Balance</span>
-            <p class="acc-amt">
-              <ion-text color="white">Rs 80232000</ion-text>
-            </p>
-            <span>9123134130</span>
-          </ion-label>
-        </swiper-slide>
-      </swiper>
-      <!-- </ion-row> -->
+          <swiper-slide>
+            <ion-label>
+              <span>Total Balance</span>
+              <p class="acc-amt">
+                <ion-text color="white">Rs 80232000</ion-text>
+              </p>
+              <span>9123134130</span>
+            </ion-label>
+          </swiper-slide>
+          <!-- </div> -->
+        </swiper>
+      </ion-row>
+      
     </ion-content>
   </ion-page>
 </template>
@@ -70,9 +73,12 @@ import {
   IonContent,
   IonText,
   IonAvatar,
+  IonRow,
 } from "@ionic/vue";
 
 export default defineComponent({
+  
+  
   components: {
     IonPage,
     IonToolbar,
@@ -84,7 +90,11 @@ export default defineComponent({
     IonAvatar,
     Swiper,
     SwiperSlide,
+    IonRow,
   },
+
+  
+
 });
 </script>
 
@@ -112,6 +122,51 @@ ion-toolbar {
     height: 2.5rem;
     width: 2.5rem;
     border: 1px solid var(--ion-color-tertiary);
+    img {
+      max-width: 100%;
+    }
   }
+}
+
+ion-content {
+  ion-row.banners {
+    position: relative;
+    padding: 1.5rem;
+    height: 34vh;
+    background: linear-gradient(
+      120.67deg,
+      #03b5aa 17.45%,
+      rgba(162, 167, 127, 0.09) 105.58%
+    );
+    border-radius: 2.5rem;
+    margin: 6vh 1rem 0 1rem;
+
+    swiper {
+      swiper-slide {
+        ion-label {
+          p.acc-amt {
+            font-size: 3rem;
+            margin: 1rem 0;
+            font-weight: 700;
+          }
+        }
+      }
+    }
+  }
+}
+
+
+ion-row.banners::after{
+  content: "";
+  position: absolute;
+  top:1px;
+  left:1px;
+  right:1px;
+  bottom:1px;
+  background: #194648;
+  border-radius: 2.5rem;
+  z-index:0;
+  border: 2px solid transparent;
+
 }
 </style>
